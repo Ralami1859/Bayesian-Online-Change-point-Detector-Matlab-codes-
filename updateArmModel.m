0f8289f0e0051a69b6b8bde82957e19087332deb
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-06-02 12:58:48 +0200

    Delete updateArmModel.m

--- updateArmModel.m
+++
@@ -1,7 +0,0 @@
-function [alphas, betas] = updateArmModel(alphas, betas, reward, alpha0, beta0)
-    alphas = alphas + reward;
-    betas = betas + (1-reward);
-    alphas = [alphas alpha0];
-    betas = [betas beta0];
-end
-    
